6600d0e5a1e5fe0604a37ba783bf0eb8e7c1ed3e
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-02-07 14:23:58 -0700

    Initial session setup

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("express-async-errors");
+require("dotenv").config();
+
+const app = express();
+
+// Session config
+const session = require("express-session");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.set("view engine", "ejs");
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+// secret word handling
+// let secretWord = "syzygy";
+app.get("/secretWord", (req, res) => {
+  // Set the user session keyword
+  if (!req.session.secretWord) {
+    req.session.secretWord = "syzygy";
+  }
+  res.render("secretWord", { secretWord: req.session.secretWord });
+});
+app.post("/secretWord", (req, res) => {
+  req.session.secretWord = req.body.secretWord;
+  res.redirect("/secretWord");
+});
+
+app.use((req, res) => {
+  res.status(404).send(`That page (${req.url}) was not found.`);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+  console.log(err);
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
